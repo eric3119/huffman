73a64a76c0072528dce05fdcef603f0dc1789875
ericCoelho <eric.s.coelho@gmail.com>
2018-04-21 08:02:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char* byte;
+}binary; 
+
+typedef struct hash_table{
+	binary* bytes[256];
+}hash_table;
 
 typedef struct NODE{
 	unsigned char byte;
 	int freq;
-	struct NODE *next, *prev;
+	struct NODE *next;
 	struct NODE *left, *right;
 }NODE;
 
 typedef struct FILA{
-	NODE *head, *tail;
+	NODE *head;
 }FILA;
 
-#include "func_lista.h"
 #include "func_fila.h"
 #include "func_arvore.h"
+#include "hash.h"
+#include "comprimir.h"
 
 void print(NODE *p){
 	if(!p) return;
@@ -34,40 +44,64 @@ void print_pre(NODE *n){
 int main(int argc, char const *argv[]){
 
 	if(argc == 1){
-		puts("erro");
+		puts("erro nome arquivo");
 		return 0;
 	}
 
 	FILE *arquivo = fopen(argv[1], "rb");
 	
 	if(!arquivo){
-		puts("erro");
+		puts("erro abrir arquivo");
 		return -1;
-	}else 
-		printf("Comprimindo -> %s\n",argv[1]);
-	
-	unsigned char c;	
-	FILA *fila = create_fila();
+	}
+	int i, tb_freq[256];
+	unsigned char c;
+	memset(tb_freq, 0, sizeof(tb_freq));
+
+	FILA *fila = create_fila();	
 	
 	fread(&c, sizeof(c), 1, arquivo);
 
 	while(!feof(arquivo)){		
-		enqueue(fila, c);
+		tb_freq[c]++;
 		fread(&c, sizeof(c), 1, arquivo);	
+	}
+
+	for(i=0; i<256; i++){
+		if(tb_freq[i]){
+			NODE *novo = create_node(tb_freq[i],i);
+			enqueue(fila, novo);
+		}
 	}
 	
 	fclose(arquivo);
-
-	while(!empty_queue(fila)){
+	puts("fila:");
+	print_fila(fila);
+	while(1){
 		NODE *n1 = dequeue(fila),
 			 *n2 = dequeue(fila);
+
+		NODE *novo = criar_arvore(n1, n2);
+		
 		if(empty_queue(fila)){
-			enqueue_arvore(f, n1, n2);
+		
+			enqueue(fila, novo);
 			break;
 		}
+		enqueue(fila, novo);		
 	}
+	puts("arvore:");
+	print_arvore(fila->head);puts("");
+	
+	hash_table* ht = criar_hash();
 
-	printf("%d\n", size_fila(fila));
-
+	int tam = height(fila->head);
+	char* byte = (char*)malloc((tam+1)*sizeof(char));
+	
+	search(fila->head,byte,ht, 0);
+	print_hash(ht);
+		
+	comprimir(ht, argv[1]);
+	
 	return 0;
 }
