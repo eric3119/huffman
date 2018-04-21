5db6dc8cbf6e5b098d332da293c119cbdaa604b0
ericCoelho <eric.s.coelho@gmail.com>
2018-04-21 08:04:21 -0300

    Update func_fila.h

--- func_fila.h
+++ func_fila.h
@@ -1,54 +1,63 @@
+void print_fila(FILA *fila);
+
 FILA* create_fila(){
 	FILA *novo = (FILA*) malloc(sizeof(FILA));
 
-	novo->head = novo->tail = NULL;
+	novo->head = NULL;
 
 	return novo;
 }
 
-int size_fila(FILA *f){
-	NODE *aux = f->head;
-	int cont = 0;
-	while(aux){
-		cont++;
-		aux = aux->next;
-	}
-	return cont;
+int empty_queue(FILA *fila){
+	return (fila->head)? 0:1;
 }
 
-int empty_queue(FILA *f){
-	return (f->head)? 0:1;
-}
+NODE* dequeue(FILA *fila){
+	
+	if(empty_queue(fila)) return NULL;
+	
+	NODE *aux = fila->head;
 
-NODE* dequeue(FILA *f){
-	if(empty_queue(f)) return NULL;
-
-	NODE *aux = f->head;
-
-	f->head = f->head->next;
-
+	fila->head = fila->head->next;
+	
+	aux->next = NULL;
+	
 	return aux;
 }
 
-void enqueue(FILA *fila, unsigned char c){
-	if(!encontrar_byte(fila->head, c)){
+void enqueue(FILA *fila, NODE *n){
 
-		NODE *novo = (NODE*) malloc(sizeof(NODE));
-
-			novo->byte = c;
-			novo->freq = 1;
-			novo->next = novo->prev = NULL;
-			novo->left = novo->right = NULL;
-			novo->next = novo->prev = NULL;
-
-		if(empty_queue(fila)){
-			fila->head = fila->tail = novo;
-		}else{
-			novo->prev = fila->head;
-			fila->head->next = novo;
-			fila->head = novo;
-		}
+	NODE *atual = fila->head,
+	     *ant = NULL;
+	while((atual!= NULL)&&(atual->freq < n->freq))	{
+		ant = atual;
+		atual = atual->next;
+	}
+	if(ant== NULL){
+		n->next = fila->head;
+		fila->head = n;
+	}else{
+		ant->next = n;
+		n->next = atual;
 	}
 }
 
-void print_fila
+void print_fila(FILA *fila){
+	NODE *aux = fila->head;
+
+	while(aux){
+		if(aux->byte != '\n') printf("%c, %d\n", aux->byte, aux->freq);		
+		else printf("\\n, %d\n", aux->freq);
+		aux = aux->next;
+	}
+}
+
+NODE* create_node(int freq, unsigned char c){
+	NODE *novo = (NODE*) malloc(sizeof(NODE));
+
+		novo->byte = c;
+		novo->freq = freq;
+		novo->left = novo->right = novo->next = NULL;
+
+	return novo;
+}
